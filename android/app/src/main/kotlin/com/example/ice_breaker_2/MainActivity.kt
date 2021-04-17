ba209c731e45d0a89df59cfa22ad434bb56416bc
Your Name <you@example.com>
2021-04-18 01:35:13 +0530

    First commit of 2nd page

---
+++ android/app/src/main/kotlin/com/example/ice_breaker_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ice_breaker_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Explore",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Explore')),
+        body: BodyLayout(),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text("home"),
+                backgroundColor: Colors.blue
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                title: Text("search"),
+                backgroundColor: Colors.blue
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.camera),
+                title: Text("camera"),
+                backgroundColor: Colors.blue
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BodyLayout extends StatelessWidget {
+@override
+Widget build(BuildContext context) {
+  return _myListView(context);
+}
+}
+
+// replace this function with the code in the examples
+Widget _myListView(BuildContext context) {
+  return ListView(
+    children: <Widget>[
+      ListTile(
+        leading: CircleAvatar(
+          backgroundImage: AssetImage('assets/ad.jpg'),
+        ),
+        title: Text('Aryan Dhankani'),
+        trailing: Icon(Icons.keyboard_arrow_right),
+        onTap: () {
+        print('ad');}
+      ),
+      ListTile(
+        leading: CircleAvatar(
+          backgroundImage: AssetImage('assets/dm.jpg'),
+        ),
+        title: Text('Dhruv Mehrotra'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            print('dm');}
+      ),
+      ListTile(
+        leading: CircleAvatar(
+          backgroundImage: AssetImage('assets/st.jpg'),
+        ),
+        title: Text('Saumya Talera'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            print('st');}
+      ),
+      ListTile(
+        leading: CircleAvatar(
+          backgroundImage: AssetImage('assets/an.jpg'),
+        ),
+        title: Text('Aman_1'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            print('an');}
+      ),
+      ListTile(
+      leading: CircleAvatar(
+          backgroundImage: AssetImage('assets/an.jpg'),
+        ),
+          title: Text('Aman_2'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+           onTap: () {
+            print('an');}
+      ),
+      ListTile(
+        leading: CircleAvatar(
+          backgroundImage: AssetImage('assets/an.jpg'),
+        ),
+        title: Text('Anand'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            print('an');}
+      ),
+      ListTile(
+        leading: CircleAvatar(
+          backgroundImage: AssetImage('assets/tn.jpg'),
+        ),
+        title: Text('Tanya'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            print('tn');}
+      ),
+      ListTile(
+        leading: CircleAvatar(
+          backgroundImage: AssetImage('assets/mz.jpg'),
+        ),
+        title: Text('Mark Zukerberg'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            print('mz');}
+      ),
+      ListTile(
+        leading: CircleAvatar(
+          backgroundImage: AssetImage('assets/ad.jpg'),
+        ),
+        title: Text('Aditya'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            print('ad');}
+      ),
+    ],
+  );
+}
